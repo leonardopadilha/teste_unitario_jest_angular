0d1179a036b5a67255b4c7926212feb223e051bb
leonardopadilha <leonardopadilha28@gmail.com>
2025-02-06 05:20:48 -0300

    Validando no teste a criação do componente alt e src corretamente

--- src/app/componentes/cabecalho/cabecalho.component.spec.ts
+++ src/app/componentes/cabecalho/cabecalho.component.spec.ts
@@ -1,17 +1,24 @@
-import { TestBed } from "@angular/core/testing"
+import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { CabecalhoComponent } from "./cabecalho.component"
 
 describe('CabecalhoComponent', () => {
   let component: CabecalhoComponent
+  let fixture: ComponentFixture<CabecalhoComponent>
 
   beforeEach(() => {
     TestBed.configureTestingModule({ // Pacote Angular
       imports: [CabecalhoComponent],
     })
-    component = new CabecalhoComponent()
+    fixture = TestBed.createComponent(CabecalhoComponent)
+    component = fixture.componentInstance
   })
 
   it('deve ser criado com sucesso', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('deve definir as propriedades alt e src', () => {
+    expect(component.alt).toBeDefined()
+    expect(component.src).toBeDefined()
   })
 })
